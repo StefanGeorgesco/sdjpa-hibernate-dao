fd4700ee6c6a011f7c6f8bbcca620f7db85f31e0
Stefan Georgesco <stefan.georgesco@yahoo.fr>
2023-04-11 10:28:56 +0200

    Applied my preferences

--- src/test/java/guru/springframework/jdbc/DaoIntegrationTest.java
+++ src/test/java/guru/springframework/jdbc/DaoIntegrationTest.java
@@ -1,17 +1,14 @@
 package guru.springframework.jdbc;
 
 import guru.springframework.jdbc.dao.AuthorDao;
-import guru.springframework.jdbc.dao.AuthorDaoImpl;
 import guru.springframework.jdbc.dao.BookDao;
-import guru.springframework.jdbc.dao.BookDaoImpl;
 import guru.springframework.jdbc.domain.Author;
 import guru.springframework.jdbc.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
@@ -23,7 +20,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
  */
 @ActiveProfiles("local")
 @DataJpaTest
-@Import({AuthorDaoImpl.class, BookDaoImpl.class})
+@ComponentScan(basePackages = {"guru.springframework.jdbc.dao"})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class DaoIntegrationTest {
     @Autowired
@@ -34,7 +31,7 @@ public class DaoIntegrationTest {
 
     @Test
     void testListAuthorByLastNameLike() {
-        List<Author> authors = authorDao.listAuthorByLastNameLike("Wall");
+        List<Author> authors = authorDao.listAuthorByLastNameLike("all");
 
         assertThat(authors).isNotNull();
         assertThat(authors.size()).isGreaterThan(0);
@@ -62,9 +59,6 @@ public class DaoIntegrationTest {
         book.setPublisher("Self");
         book.setTitle("my book");
 
-        Author author = new Author();
-        author.setId(3L);
-
         book.setAuthorId(1L);
         Book saved = bookDao.saveNewBook(book);
 
@@ -83,10 +77,7 @@ public class DaoIntegrationTest {
         book.setPublisher("Self");
         book.setTitle("my book");
 
-        Author author = new Author();
-        author.setId(3L);
-
-        book.setAuthorId(1L);
+        book.setAuthorId(3L);
         Book saved = bookDao.saveNewBook(book);
 
         assertThat(saved).isNotNull();
@@ -120,7 +111,7 @@ public class DaoIntegrationTest {
         Author deleted = authorDao.getById(saved.getId());
         assertThat(deleted).isNull();
 
-        assertThat(authorDao.getById(saved.getId()));
+        assertThat(authorDao.getById(saved.getId())).isNull();
 
     }
 
